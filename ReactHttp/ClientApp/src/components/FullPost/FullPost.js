c529a15fd47192c86f445c370879cccbbf0c02e9
Ivan Tabarino <tabarino@outlook.com>
2021-04-12 23:08:22 +0100

    Fetching Data on Update (without creating Infinite Loops)

--- ReactHttp/ClientApp/src/components/FullPost/FullPost.js
+++ ReactHttp/ClientApp/src/components/FullPost/FullPost.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import styles from './FullPost.module.css';
 
-const FullPost = (props) => {
-  let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-
-  if (props.id) {
-    post = (
-      <div className={styles.FullPost}>
-        <h1>Title</h1>
-        <p>Content</p>
-        <div className={styles.Edit}>
-          <button className={styles.Delete}>Delete</button>
-        </div>
-      </div>
-    );
+export class FullPost extends Component {
+  state = {
+    loadedPost: null
   }
 
-  return post;
+  componentDidUpdate() {
+    if (!this.props.id) {
+      return;
+    }
+
+    if (!this.state.loadedPost
+    ||  this.state.loadedPost && this.state.loadedPost.id !== this.props.id) {
+      axios.get('/api/posts/' + this.props.id)
+        .then(response => {
+          this.setState({ loadedPost: response.data });
+        });
+    }
+  }
+
+  render() {
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+
+    if (this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+    }
+
+    if (this.state.loadedPost) {
+      post = (
+        <div className={styles.FullPost}>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className={styles.Edit}>
+            <button className={styles.Delete}>Delete</button>
+          </div>
+        </div>
+      );
+    }
+
+    return post;
+  }
 }
 
 export default FullPost;
