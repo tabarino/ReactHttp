0343f57ad77f0cb240594d4bb5972cee49aec993
Ivan Tabarino <tabarino@outlook.com>
2021-04-28 16:07:03 +0100

    Redirect Requests

--- ReactHttp/ClientApp/src/containers/Blog/Blog.js
+++ ReactHttp/ClientApp/src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
 // import FullPost from '../FullPost/FullPost';
 import styles from './Blog.module.css';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 
 class Blog extends Component {
@@ -29,6 +29,7 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
           {/* <Route path="/:id" exact component={FullPost} /> */}
         </Switch>
       </div>
